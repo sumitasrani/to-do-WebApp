48b1342d635aa6bb407eb6632e679fef82d5de89
sumitasrani <hello@sumitasrani.com>
2023-08-01 00:35:44 +0530

    Initial commit: todo list with download button

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("My-To-do App")
+st.subheader("My web-based to-do app")
+st.write("Focus. Streamline. Prioritize. Increase your daily productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add to-do...",
+              on_change=add_todo, key='new_todo')
+
+with open("todos.txt", "r") as file:
+    download_btn = st.download_button(
+            label="Download to-do list",
+            data=file,
+            file_name="My_todo_list.txt",
+            mime="text/plain"
+          )
